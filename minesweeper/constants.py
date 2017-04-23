95efdc9f4a3b2d12c7e0c026fa3ae63c9fa74eca
ieatkittens <samuelm@gadventures.com>
2017-04-23 12:39:05 -0700

    Fixed some punctuation

--- minesweeper/constants.py
+++ minesweeper/constants.py
@@ -9,14 +9,14 @@ SUCCESS_MESSAGES = [
     'Looks like all that beach combing really paid off!',
     'You also found $3.75 in loose change!',
     'Maybe it\'s time to increase that mine count?',
-    'Does this mean the fireworks are cancelled?'
-    'There\'s no better reward than not getting exploded'
+    'Does this mean the fireworks are cancelled?',
+    'There\'s no better reward than not getting exploded!'
 ]
 
 FAILURE_MESSAGES = [
     'On the upside, now no one else will step on that mine!',
     'No one else would know if you hit undo...but you would.',
     'Maybe you\'re more suited to a desk job?',
-    'If it\'s any consolation, you didn\'t feel a thing',
+    'If it\'s any consolation, you didn\'t feel a thing.',
     'Risky click of the day?'
 ]
